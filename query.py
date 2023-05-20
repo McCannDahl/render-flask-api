bf37dfd5e34645d4f8c005637173bff1cdc5deea
McCann Dahl <mccann.dahl@parivedasolutions.com>
2023-05-19 21:30:58 -0500

    remove models

--- query.py
+++ query.py
@@ -1,32 +1,35 @@
 
-from haystack.pipelines import ExtractiveQAPipeline, DocumentSearchPipeline, GenerativeQAPipeline
-from shared import reader, retriever, generator
+# from haystack.pipelines import ExtractiveQAPipeline, DocumentSearchPipeline, GenerativeQAPipeline
+# from shared import reader, retriever, generator
 
-querying_pipeline = ExtractiveQAPipeline(reader, retriever)
-generative_pipeline = GenerativeQAPipeline(generator, retriever)
-search_pipe = DocumentSearchPipeline(retriever)
-params = {
-    "Retriever": {"top_k": 5},
-}
+# querying_pipeline = ExtractiveQAPipeline(reader, retriever)
+# generative_pipeline = GenerativeQAPipeline(generator, retriever)
+# search_pipe = DocumentSearchPipeline(retriever)
+# params = {
+#     "Retriever": {"top_k": 5},
+# }
 
 def queryDocuments(input):
-    answer = search_pipe.run(
-        query=input,
-        params=params
-    )
-    return answer
+    return {}
+    # answer = search_pipe.run(
+    #     query=input,
+    #     params=params
+    # )
+    # return answer
 def queryExtractive(input):
-    answer = querying_pipeline.run(
-        query=input,
-        params=params
-    )
-    return answer
+    return {}
+    # answer = querying_pipeline.run(
+    #     query=input,
+    #     params=params
+    # )
+    # return answer
 def queryGenerative(input):
-    answer = generative_pipeline.run(
-        query=input,
-        params=params
-    )
-    return answer
+    return {}
+    # answer = generative_pipeline.run(
+    #     query=input,
+    #     params=params
+    # )
+    # return answer
 
 if __name__ == "__main__":
     input = "How can I find the truth?"
--- shared.py
+++ shared.py
@@ -9,33 +9,33 @@ APIKEY = os.environ['APIKEY']
 pineconeIndex = 'lds-test'
 pineconeEnv = "asia-southeast1-gcp-free"
 
-document_store = PineconeDocumentStore(
-    APIKEY,
-    environment=pineconeEnv,
-    similarity="cosine",
-    index=pineconeIndex,
-    embedding_dim=384,
-)
+# document_store = PineconeDocumentStore(
+#     APIKEY,
+#     environment=pineconeEnv,
+#     similarity="cosine",
+#     index=pineconeIndex,
+#     embedding_dim=384,
+# )
 
-retriever = EmbeddingRetriever(
-    document_store=document_store,
-    embedding_model="multi-qa-MiniLM-L6-cos-v1",
-    model_format="sentence_transformers"
-)
+# retriever = EmbeddingRetriever(
+#     document_store=document_store,
+#     embedding_model="multi-qa-MiniLM-L6-cos-v1",
+#     model_format="sentence_transformers"
+# )
 
-preprocessor = PreProcessor(
-    clean_whitespace=True,
-    clean_header_footer=True,
-    clean_empty_lines=True,
-    split_by="word",
-    split_length=200,
-    split_overlap=20,
-    split_respect_sentence_boundary=True,
-)
+# preprocessor = PreProcessor(
+#     clean_whitespace=True,
+#     clean_header_footer=True,
+#     clean_empty_lines=True,
+#     split_by="word",
+#     split_length=200,
+#     split_overlap=20,
+#     split_respect_sentence_boundary=True,
+# )
 
-reader = FARMReader(
-    model_name_or_path='deepset/electra-base-squad2', 
-    use_gpu=True
-)
+# reader = FARMReader(
+#     model_name_or_path='deepset/electra-base-squad2', 
+#     use_gpu=True
+# )
 
-generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa")
+# generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa")
